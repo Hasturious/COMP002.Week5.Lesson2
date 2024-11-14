29c8658a45794113ebe1d38d1605d876f7597007
Jean Figeuroa <jbster559@gmail.com>
2024-11-13 23:58:22 -0800

    Debugging

---
+++ script.js
@@ -0,0 +1,14 @@
+/*
+const button = document.querySelector("button");
+
+button.addEventListener("click", function() {
+    alert("Button was clicked!");
+});
+
+const a = document.querySelector("a")
+
+a.addEventListener("click", function(event) {
+    event.preventDefault();
+    alert("Get back to work!")
+});
+*/
